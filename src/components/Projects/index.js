3e33482e2576394526918e7c875ea8c55b33848a
Ruben Quispe <rwquispec@uni.pe>
2021-06-06 01:36:52 -0500

    section project is responsive

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,20 +1,66 @@
 import React from 'react';
 import './index.css'
 import defaultImage from './project.png'
+import {
+	Grid,
+	Row,
+	Column
+} from 'carbon-components-react'
+
+const data = [
+	{
+		title: 'Coming soon.',
+		href: '',
+		message: 'Coming soon I\'ll upload my projects.'
+	},
+	{
+		title: 'Coming soon.',
+		href: '',
+		message: 'Coming soon I\'ll upload my projects.'
+	},
+	{
+		title: 'Coming soon.',
+		href: '',
+		message: 'Coming soon I\'ll upload my projects.'
+	},
+	{
+		title: 'Coming soon.',
+		href: '',
+		message: 'Coming soon I\'ll upload my projects.'
+	}
+]
 
 const Projects = () => (
 	<div className="projects" id="projects">
 		<div className="projects__title">
 			<h3>&lt; projects &gt;</h3>
 		</div>
-		<div className="card__project">
-			<img src={defaultImage} alt="project photo"/>
-			<div className="card__project-content">
-				<p>Coming soon I'll upload my projects.</p>
-				<a className="know-more">Source</a>
-			</div>
-			<div className="card__project-title">Coming soon.</div>
-		</div>
+		<Grid>
+			<Row>
+				{data.map(({title, href, message}, index) => (
+					<Column md={4} lg={3}>
+						<div className="card__project">
+							<Grid>
+								<Row>
+									<Column style={{position: 'relative', padding: 0}}>
+										<img src={defaultImage} alt="project photo" className="card__project-image"/>
+										<div className="card__project-content">
+											<p>{message}</p>
+											<a className="know-more">Source</a>
+										</div>
+									</Column>
+								</Row>
+								<Row>
+									<Column style={{padding: 0}}>
+										<div className="card__project-title">{title}</div>
+									</Column>
+								</Row>
+							</Grid>
+						</div>
+					</Column>
+				))}
+			</Row>
+		</Grid>
 	</div>
 )
 
